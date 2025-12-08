513d83bcc7aab581bb1a6feab646a4182b0a5611
Alejandro <elaeon@gmail.com>
2025-12-08 16:43:54 -0600

    update scripts and sts

--- scripts/dv_cleaner.py
+++ scripts/dv_cleaner.py
@@ -7,15 +7,22 @@ from collections import defaultdict
 import argparse
 
 
+_log = logging.getLogger(__name__)
+_log.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+_log.addHandler(handler)
+
+
 def extract_index(page_text):
     scope_index = None
     lines_dict = defaultdict(dict)
     index_pat = re.compile(r"^(?P<index>\d{3}(?:-(\d+)){0,1})")
     tollbooth_pat = re.compile(r"caseta:(?P<tollbooth_name>(.*))")
-    strech_pat = re.compile(r"^movimiento:(?P<direction>\w+(\s\w+)*(?:-\w+(\s\w+)*))\s(mex){0,1}")
-    road_pat = re.compile(r"(?P<road>mex(?:-(\w+))+?)\s*km:(?P<place>\d+\.\d+)")
+    strech_pat = re.compile(r"^movimiento:(?P<direction>\w+[.]*(\s\w+)*(?:\s*-{0,1}\s*\w+[.]*(\s\w+[.]*)*))\s[mex|slp|chh|ver|nl]")
+    road_pat = re.compile(r"[mex|ver|slp|chh|nl][-]{0,1}(?P<road>\w*)\s*km:(?P<km>\d+\.\d+)")
     for i, line in enumerate(page_text.split("\n"), 1):
-        line = line.lower()
+        line = line.lower().replace("(", "").replace(")", "")
         match_index = re.search(index_pat, line)
         match_tb = re.search(tollbooth_pat, line)
         match_strech = re.search(strech_pat, line)
@@ -25,12 +32,12 @@ def extract_index(page_text):
             scope_index = index_dict["index"]
             if match_tb is not None:
                 lines_dict[scope_index].update(
-                    {"tollbooth_name": match_tb.groupdict()["tollbooth_name"].replace("coordenadas", "")}
+                    {"tollbooth_name": match_tb.groupdict()["tollbooth_name"].replace("coordenadas", "").strip()}
                 )
-        elif match_strech is not None and scope_index is not None:
+        if match_strech is not None and scope_index is not None:
             lines_dict[scope_index].update(match_strech.groupdict())
-            if match_road is not None:
-                lines_dict[scope_index].update(match_road.groupdict())
+        if match_road is not None and scope_index is not None:
+            lines_dict[scope_index].update(match_road.groupdict())
 
     return lines_dict
 
@@ -45,35 +52,58 @@ def lines_dict_to_df(page_text):
     df = pl.DataFrame(columns, orient="row")
     return df
 
-    
-def main():
+
+def fill_list(small_list, total_size):
+    num_spaces = total_size - len(small_list)
+    filled_list = []
+    for _ in range(num_spaces):
+        filled_list.append("")
+    filled_list.extend(small_list)
+    return filled_list
+
+
+def main(year, from_page, to_page):
     year = 2025
     file_path = f"./datos_viales/{year}/33_DV{year}_PlazasCobro{year}.pdf"
     with pdfplumber.open(file_path) as pdf:
-        for i, page in enumerate(pdf.pages, 1):
-            if i >= 55:
+        all_df = []
+        for page_num, page in enumerate(pdf.pages, 1):
+            if page_num >= from_page:
                 extracted_tables = page.extract_tables()
-                page_text = page.extract_text()
+                page_text = page.extract_text(keep_blank_chars=True)
                 df_index = lines_dict_to_df(page_text)
+                assert df_index.columns == ['index', 'tollbooth_name', 'direction', 'road', 'km']
                 dfs = []
-                for table in extracted_tables:
+                for table_num, table in enumerate(extracted_tables, 1):
                     tdpa = table[1][0]
                     transport_type = table[1][1].split(" ")
                     vta = table[1][2]
                     volume_month = table[1][3].split(" ")
                     transport_sts = table[2][1].split(" ")
                     volume_sts = table[2][3].split(" ")
+                    if len(volume_sts) < len(volume_month):
+                        _log.info(volume_sts)
+                        volume_sts = fill_list(volume_sts, len(volume_month))
+                        _log.info(volume_sts)
                     header = ["tdpa"] + transport_type + ["vta"] + volume_month
                     body = [tdpa] + transport_sts + [vta] + volume_sts
-                    dfs.append(pl.DataFrame([body], schema=header, orient="row"))
+                    try:
+                        dfs.append(pl.DataFrame([body], schema=header, orient="row"))
+                    except pl.exceptions.ShapeError:
+                        _log.info(f"Dataframe shape error in page: {page_num}, table: {table_num}")
                 df_sts = pl.concat(dfs)
-                #print(df)
-                #print(df_index)
                 df = pl.concat([df_index, df_sts], how="horizontal")
-                #print(df)
-                if i == 60:
+                _log.info(f"page: {page_num}, df shape: {df.shape}")
+                all_df.append(df)
+                if page_num == to_page:
                     break
+        pl.concat(all_df).write_csv("tollbooths_sts.csv")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--year", help="filename year", required=True, type=int)
+    parser.add_argument("--from-page", help="parse from page", required=False, type=int, default=55)
+    parser.add_argument("--to-page", help="parse to page", required=False, type=int, default=200)
+    args = parser.parse_args()
+    main(year=args.year, from_page=args.from_page, to_page=args.to_page)
