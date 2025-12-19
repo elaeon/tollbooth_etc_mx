3c73ba8fce229fda48d5d9203a80e2f5c166abaf
Alejandro <elaeon@gmail.com>
2025-12-19 12:08:13 -0600

    add tb request on map

--- tb_map_editor/main.py
+++ tb_map_editor/main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, Body
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from sqlmodel import select
 
 from .model import Tollbooth
 from contextlib import asynccontextmanager
 from .utils.connector import SessionDep, create_db_and_tables
 
-from typing import Annotated
+from typing import Annotated, Any
 
 
 @asynccontextmanager
@@ -29,20 +31,30 @@ def map_root(request: Request):
     )
 
 
-@app.post("/tollbooth/")
-#def create_tollbooth(tollbooth: Tollbooth, session: SessionDep) -> Tollbooth:
-def create_tollbooth(tollbooth_name: Annotated[str, Form()], coords: Annotated[str, Form()], session: SessionDep):
-    tollbooth = Tollbooth(
-        tollbooth_name=tollbooth_name, coords=coords, status="open", state="", place="", lines=0, type="toll" 
-        )
-    session.add(tollbooth)
-    session.commit()
-    session.refresh(tollbooth)
-    return tollbooth
+# @app.post("/tollbooth/")
+# #def create_tollbooth(tollbooth: Tollbooth, session: SessionDep) -> Tollbooth:
+# def create_tollbooth(tollbooth_name: Annotated[str, Form()], coords: Annotated[str, Form()], session: SessionDep):
+#     tollbooth = Tollbooth(
+#         tollbooth_name=tollbooth_name, coords=coords, status="open", state="", place="", lines=0, type="toll" 
+#         )
+#     session.add(tollbooth)
+#     session.commit()
+#     session.refresh(tollbooth)
+#     return tollbooth
 
 
-@app.get("/tollbooth/")
-def show_tb(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="tb.html"
-    )
+@app.post("/api/tollbooths/")
+def fetch_tollbooths(body: Annotated[Any, Body()], session: SessionDep, offset: int=0, limit: int=10):
+    param, value = body["query"].split(":")
+    stm = select(Tollbooth).where(getattr(Tollbooth, param) == value)
+    tollbooths = session.exec(stm.offset(offset).limit(limit))
+    data = []
+    for tb in tollbooths:
+        lat, lon = map(lambda x: x.strip(), tb.coords.split(','))
+        data.append({
+            "tollbooth_id": tb.tollbooth_id,
+            "tollbooth_name": tb.tollbooth_name,
+            "lat": lat,
+            "lon": lon,
+        })
+    return data
