57d33831a40c6b5c4c2be6c5f75953f976903145
Alejandro <elaeon@gmail.com>
2025-12-28 17:44:17 -0600

    fix colum name in tb and add script

---
+++ scripts/create_data.sh
@@ -0,0 +1,7 @@
+uv run scripts/dv_cleaner.py --year 2025 --from-page 55
+uv run scripts/dv_cleaner.py --year 2024 --from-page 55
+uv run scripts/dv_cleaner.py --year 2023 --from-page 54
+uv run scripts/dv_cleaner.py --year 2022 --from-page 53
+uv run scripts/dv_cleaner.py --year 2021 --from-page 51
+uv run scripts/dv_cleaner.py --year 2020 --from-page 51
+uv run scripts/dv_cleaner.py --year 2019 --from-page 51
